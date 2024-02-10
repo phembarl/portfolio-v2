03b5f4769c3edb47ce52b506099de1ba78adff72
Femi Balogun <phembarl@yahoo.com>
2024-02-10 18:51:14 +0100

    add og-image, keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,15 @@ import { quicksand } from '../styles/fonts';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import MobileNav from '@/components/Navbar/MobileNav';
+import { keywords } from '@/data';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://femibalogun.dev'),
   title: 'Femi Balogun',
   description:
     'Femi is a seasoned Software Engineer with about 6 years of experience actively engaged in full product life cycles, resulting in the successful launch of various applications',
   applicationName: 'Femi Balogun',
-  keywords: [],
+  keywords,
 };
 
 export default function RootLayout({
